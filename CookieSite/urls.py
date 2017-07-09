983b4966bce14c523d3bcb64d5368ae033d12b77
suebinmon <jg0100290@gmail.com>
2017-07-10 02:51:38 +0900

    Photo field 추가

--- CookieSite/urls.py
+++ CookieSite/urls.py
@@ -14,9 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
 from django.conf.urls import include, url
+from django.conf.urls.static import static
 from django.contrib import admin
+from django.conf import settings
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^board/', include('board.urls', namespace='board')),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
